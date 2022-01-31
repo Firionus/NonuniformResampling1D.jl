72e3b485d99f6a8672950b77e8dca0a5b68cb541
Johannes Fried <jcmf.schule@gmail.com>
2022-02-01 00:56:59 +0100

    Realize the Algorithm Like This Isn't Local
    It's probably better to first upsample to a linear grid of high enough
    resolution, then downsample to the arbitrary grid. That way, we can get
    higher modularity and probably significant speed-ups. Because
    interpolating arbitrarily sampled data is non-local, i.e. a far away
    point can have large impact and for each interpolated point we'd have to
    consider all original points, which is not efficient.

--- src/Regrid1D.jl
+++ src/Regrid1D.jl
@@ -16,6 +16,35 @@ HannBasis(width=1.) = FiniteBasisFunction(x -> begin
     cos(pi*x/2/width)^2
 end, width)
 
+# Rough layout of algorithm
+# =========================
+
+# determine slice (a slice is a space between two output nodes)
+# needed for slice: xstart, xstop (with xstop > xstart (check this condition holds!)), out_ind_start, out_ind_stop
+# at the beginning and end: assume slice is as big as next one
+
+# determine involved input nodes (throw if non-existent, don't assume any boundary condition)
+
+# determine linear grid needed for slice
+# slice_grid_step = min(slice_width/min_downsampling, min(involved_input_nodes_steps)/min_upsampling)
+
+# high resolution linear grid x-positions
+# starting 1/2 step next to xstart
+
+# resample to high resolution linear grid with upsampling_function
+
+# for left node of slice
+# weight high resolution points with downsampling_function and accumulate while also accumulating the sum of the weights
+# normalize sum such that sum of weight is .5
+# add to previous value of left node
+
+# for right node of slice: as left node, but assign instead of adding to previous value
+
+# repeat until we're through
+
+
+
+# TODO document requirement for xin/xout to be ascending
 function regrid1d(xin, yin, xout; 
     min_upsampling = 4, 
     min_downsampling = 4,
@@ -25,14 +54,23 @@ function regrid1d(xin, yin, xout;
     # allocate
     yout = Array{Float64, 1}(undef, length(xout))
 
-    # TODO upsample input by min_upsampling with upsampling_function (own function)
+    # virtual first slice
+    slice_width = xout[2] - xout[1]
+    xstart = xout[1] - slice_width
+    xstop = xout[1]
+    out_ind_start = 0 # doesn't exist
+    out_ind_stop = 1
 
-    # TODO iterate over the spaces between output points, 
-    # taking the average between the left and right space to each output point
+    while true
+        # determine input nodes involved in slice 
+        
+        # damn, this is gonna be really hard because how do we know there isn't
+        # some point out really far that has a large distance to its neighbor
+        # and therefore creates a far-reaching effect all the way to us? (for
+        # basis width > 1)
 
-    # in each inter-space check that sample rate is higher than min_downsampling
-    # otherwise upsample again with upsampling_function
-    # then take mean of points weighted with downsampling_function
+
+    end
 
     println("hello world - this is WIP")
 end

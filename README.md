58d0b2665263f558779fcadd46535ef55400ff39
Johannes Fried <jcmf.schule@gmail.com>
2022-01-31 21:38:34 +0100

    Write Down First Ideas

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Regrid1D
+# Regrid1D.jl
+
+Resample 1-dimensional data from arbitrary grids onto arbitrary grids in Julia.
+
+## Status
+
+**Work in Progress**: The package is not functional yet. 
+
+If you are interested in collaborating, please open an issue. 
+
+## Goals
+
+- High quality resampling from arbitrary 1D grids to arbitrary 1D grids
+- Specific application: Resampling from linear to log grid for magnitude of FFT
+- Performance is secondary right now
+
+## Approach
+
+- Use distance to next grid point as a kind of "local sample rate" with which to
+  scale basis functions for interpolation/smoothing. Treat left and right
+  neighbor separately and average between them. 
+- First perform upsampling by adding basis functions from each input point.
+  Require a certain amount of upsampling in any case. 
+- If output points are closer than input points, do even more upsampling
+- Then create output point values by weighted average where the smoothing
+  function is the weight
--- src/Regrid1D.jl
+++ src/Regrid1D.jl
@@ -1,5 +1,40 @@
 module Regrid1D
 
-# Write your package code here.
+export regrid1d, HannBasis
 
+struct FiniteBasisFunction{F<:Function, T<:Real}
+    f::F # callback x::Real -> y::Real, x ∈ [0, Inf), 
+    # y(0) should probably be 1 (current node at x=0) 
+    # y(1) should probably be 0 (next neighbor at x=1)
+    width::T # ∈ (0, Inf), width up to which the basis function will be evaluated (inclusively)
 end
+
+HannBasis(width=1.) = FiniteBasisFunction(x -> begin
+    x < 0 && error("undefined for negative values")
+    x > width && return 0.
+    # normalized integral would require pre-factor 2/width
+    cos(pi*x/2/width)^2
+end, width)
+
+function regrid1d(xin, yin, xout; 
+    min_upsampling = 4, 
+    min_downsampling = 4,
+    upsampling_function::FiniteBasisFunction = HannBasis(1.),
+    downsampling_funciton::FiniteBasisFunction = HannBasis(1.),
+    )
+    # allocate
+    yout = Array{Float64, 1}(undef, length(xout))
+
+    # TODO upsample input by min_upsampling with upsampling_function (own function)
+
+    # TODO iterate over the spaces between output points, 
+    # taking the average between the left and right space to each output point
+
+    # in each inter-space check that sample rate is higher than min_downsampling
+    # otherwise upsample again with upsampling_function
+    # then take mean of points weighted with downsampling_function
+
+    println("hello world - this is WIP")
+end
+
+end # module

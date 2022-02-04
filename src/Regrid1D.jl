2eb1ccdba68c29b5b825cec29b9c10a101210348
Johannes Fried <jcmf.schule@gmail.com>
2022-02-04 12:50:10 +0100

    First Version that Does Something
    Still need to evaluate. No upsampling.

--- src/Regrid1D.jl
+++ src/Regrid1D.jl
@@ -1,6 +1,6 @@
 module Regrid1D
 
-export regrid1d, HannBasis
+export regrid, HannBasis
 
 struct FiniteBasisFunction{F<:Function, T<:Real}
     f::F # callback x::Real -> y::Real, x ∈ [0, Inf), 
@@ -16,63 +16,117 @@ HannBasis(width=1.) = FiniteBasisFunction(x -> begin
     cos(pi*x/2/width)^2
 end, width)
 
-# Rough layout of algorithm
-# =========================
+function find_first_above_or_equal(cutoff, x::StepRangeLen)
+    # this doesn't work for all cases - assert the right ones
+    @assert Float64(x.step) > 0
+    @assert x.offset == 1
+    # basic math:
+    # y = x.ref + (i-x.offset)*x.step
+    # i = (y - x.ref)/x.step + x.offset
+    ind_float = (cutoff - x.ref)/x.step
+    ind = Int(ceil(Float64(ind_float))) + x.offset
+    @assert ind <= x.len # none of the elements are above or equal the cutoff
+    return max(1, ind)
+end
 
-# determine slice (a slice is a space between two output nodes)
-# needed for slice: xstart, xstop (with xstop > xstart (check this condition holds!)), out_ind_start, out_ind_stop
-# at the beginning and end: assume slice is as big as next one
+function find_last_below_or_equal(cutoff, x::StepRangeLen)
+    # this doesn't work for all cases - assert the right ones
+    @assert Float64(x.step) > 0
+    @assert x.offset == 1
+    # basic math:
+    # y = x.ref + (i-x.offset)*x.step
+    # i = (y - x.ref)/x.step + x.offset
+    ind_float = (cutoff - x.ref)/x.step
+    ind = Int(floor(Float64(ind_float))) + x.offset
+    @assert ind >= 1 # none of the elements are below or equal the cutoff
+    return min(x.len, ind)
+end
 
-# determine involved input nodes (throw if non-existent, don't assume any boundary condition)
-
-# determine linear grid needed for slice
-# slice_grid_step = min(slice_width/min_downsampling, min(involved_input_nodes_steps)/min_upsampling)
-
-# high resolution linear grid x-positions
-# starting 1/2 step next to xstart
-
-# resample to high resolution linear grid with upsampling_function
-
-# for left node of slice
-# weight high resolution points with downsampling_function and accumulate while also accumulating the sum of the weights
-# normalize sum such that sum of weight is .5
-# add to previous value of left node
-
-# for right node of slice: as left node, but assign instead of adding to previous value
-
-# repeat until we're through
-
-
-
-# TODO document requirement for xin/xout to be ascending
-function regrid1d(xin, yin, xout; 
-    min_upsampling = 4, 
-    min_downsampling = 4,
-    upsampling_function::FiniteBasisFunction = HannBasis(1.),
-    downsampling_funciton::FiniteBasisFunction = HannBasis(1.),
+function regrid(xin::StepRangeLen, yin, xout,
+    smoothing_function::FiniteBasisFunction = HannBasis(1.),
     )
+
+    # extent of smoothing function
+    extent = smoothing_function.width
+
+    step = xin.step|>Float64
+
     # allocate
     yout = Array{Float64, 1}(undef, length(xout))
 
+
+    out_ind = 1
+
+
     # virtual first slice
-    slice_width = xout[2] - xout[1]
-    xstart = xout[1] - slice_width
-    xstop = xout[1]
-    out_ind_start = 0 # doesn't exist
-    out_ind_stop = 1
+    slice_width = max(step, xout[2] - xout[1])
+    slice_start = xout[1] - slice_width
+    slice_stop = xout[1]
+
 
     while true
-        # determine input nodes involved in slice 
-        
-        # damn, this is gonna be really hard because how do we know there isn't
-        # some point out really far that has a large distance to its neighbor
-        # and therefore creates a far-reaching effect all the way to us? (for
-        # basis width > 1)
+        # start with left slice
+        input_start = slice_stop - slice_width*extent
+        input_stop = slice_stop
 
+        #find relevant input points
+        input_start_ind = find_first_above_or_equal(input_start, xin) # TODO if we always select or equal, we'll select values exactly on the output point twice and weigh them too heavily
+        input_stop_ind = find_last_below_or_equal(input_stop, xin)
+        # allow one single point in the window
+        @assert input_stop_ind >= input_start_ind # do we need this?
 
+        val_acc = 0.
+        win_acc = 0.
+        for input_ind in input_start_ind:input_stop_ind
+            x = xin[input_ind]
+            rel_pos = (input_stop - x)/slice_width
+            win_value = smoothing_function.f(rel_pos)
+            win_acc += win_value
+            val_acc += yin[input_ind]*win_value
+        end
+        # assing on left slice
+        yout[out_ind] = val_acc / win_acc / 2 # normalization
+
+        # left slice done
+        # now right slice
+        if out_ind < xout|>length # keep previous slice width on last element
+            slice_width = xout[out_ind + 1] - xout[out_ind] 
+        end
+        slice_start = xout[out_ind]
+        slice_stop = xout[out_ind] + slice_width
+
+        input_start = slice_start
+        input_stop = slice_start + slice_width*extent
+
+        input_start_ind = find_first_above_or_equal(input_start, xin)
+        input_stop_ind = find_last_below_or_equal(input_stop, xin)
+        # allow one single point in the window
+        @assert input_stop_ind >= input_start_ind # do we need this?
+
+        val_acc = 0.
+        win_acc = 0.
+        for input_ind in input_start_ind:input_stop_ind
+            x = xin[input_ind]
+            rel_pos = (x - input_start)/slice_width
+            win_value = smoothing_function.f(rel_pos)
+            win_acc += win_value
+            val_acc += yin[input_ind]*win_value
+        end
+        # add on right slice
+        yout[out_ind] += val_acc / win_acc / 2 # normalization
+
+        # right slice done
+        # prepare next left slice
+
+        out_ind += 1
+        out_ind > length(xout) && break
+
+        slice_width = max(step, xout[out_ind] - xout[out_ind - 1])
+        slice_start = xout[out_ind] - slice_width
+        slice_stop = xout[out_ind]
     end
 
-    println("hello world - this is WIP")
+    yout
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,41 @@
 using Regrid1D
 using Test
 
-@testset "Regrid1D.jl" begin
-    # Write your tests here.
+@testset "Regrid1D Tests" begin
+
+    @testset "find Utitilities" begin
+        @testset "find_first_above_or_equal" begin
+            x = 1.0:1.0:10.0
+            @test Regrid1D.find_first_above_or_equal(.5, x) == 1
+            @test Regrid1D.find_first_above_or_equal(0, x) == 1
+            @test Regrid1D.find_first_above_or_equal(-.5, x) == 1
+            @test Regrid1D.find_first_above_or_equal(1, x) == 1
+            @test Regrid1D.find_first_above_or_equal(1.1, x) == 2
+            @test Regrid1D.find_first_above_or_equal(5.4, x) == 6
+            @test Regrid1D.find_first_above_or_equal(9.9, x) == 10
+            @test Regrid1D.find_first_above_or_equal(10, x) == 10
+            @test_throws Exception Regrid1D.find_first_above_or_equal(10.1, x)
+        end
+
+        @testset "find_last_below_or_equal" begin
+            x = 1.0:1.0:10.0
+            @test_throws Exception Regrid1D.find_last_below_or_equal(.9, x)
+            @test Regrid1D.find_last_below_or_equal(1, x) == 1
+            @test Regrid1D.find_last_below_or_equal(1.1, x) == 1
+            @test Regrid1D.find_last_below_or_equal(5.4, x) == 5
+            @test Regrid1D.find_last_below_or_equal(9.9, x) == 9
+            @test Regrid1D.find_last_below_or_equal(10, x) == 10
+            @test Regrid1D.find_last_below_or_equal(10.1, x) == 10
+            @test Regrid1D.find_last_below_or_equal(30, x) == 10
+        end
+    end
+
+    @testset "regrid does not throw in basic example" begin
+        xin = 1.0:1.0:7.0
+        yin = [1.,2.,3.,4.,5.,6.,7.]
+        xout = [2.5, 3., 3.9]
+        regrid(xin, yin, xout)
+        @test true
+    end
+
 end

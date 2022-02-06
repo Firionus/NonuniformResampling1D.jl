57ef6681801b4011d73dbcb0cd798a65762f9839
Johannes Fried <jcmf.schule@gmail.com>
2022-02-06 21:10:48 +0100

    Implement On-Demand Upsampling

--- src/Regrid1D.jl
+++ src/Regrid1D.jl
@@ -45,53 +45,68 @@ function find_last_below(cutoff, x::StepRangeLen)
 end
 
 function regrid(xin::StepRangeLen, yin, xout,
-    smoothing_function::FiniteBasisFunction = RectBasis(.5),
+    smoothing_function::FiniteBasisFunction = RectBasis(.5);
+    required_input_points=4, upsampling_basis=HannBasis(2.) # TODO change upsampling default to Lanczos Basis
     )
-    in_step = Float64(xin.step)
     # allocate
     yout = Array{Float64, 1}(undef, length(xout))
 
     # first slice
-    right_slice_width = max(in_step, xout[2] - xout[1])
+    right_slice_width = xout[2] - xout[1]
     left_slice_width = right_slice_width
     
     out_ind = 1
     
     while true
-        yout[out_ind] = interpolate_point(xin, yin, xout[out_ind], left_slice_width, right_slice_width, smoothing_function)
+        yout[out_ind] = interpolate_point(xin, yin, xout[out_ind], left_slice_width, right_slice_width, 
+        smoothing_function, 
+        required_input_points=required_input_points, 
+        upsampling_basis=upsampling_basis)
 
         # prepare next point
         out_ind += 1
         out_ind > length(xout) && break
 
         if out_ind < length(xout) # keep previous slice width on last element
-            right_slice_width = max(in_step, xout[out_ind + 1] - xout[out_ind])
+            right_slice_width = xout[out_ind + 1] - xout[out_ind]
         end
 
-        left_slice_width = max(in_step, xout[out_ind] - xout[out_ind - 1])
+        left_slice_width = xout[out_ind] - xout[out_ind - 1]
     end
 
     yout
 end
 
-function interpolate_point(xin, yin, xpoint, left_unit_width, right_unit_width, basis::FiniteBasisFunction)
+function interpolate_point(xin, yin, xpoint, left_unit_width, right_unit_width, basis::FiniteBasisFunction;
+    required_input_points=1, upsampling_basis=missing,
+    )
     # start with left slice
     slice_width = left_unit_width
 
     input_width = slice_width*basis.width
     input_start = xpoint - input_width
     input_stop = xpoint
-    @assert input_start >= xin[1]
+    @assert input_start >= xin[1] "Not enough points at the beginning of the input"
     @assert input_stop <= xin[end] "Not enough points at the end of the input"
 
     #find relevant input points
     input_start_ind = find_first_above_or_equal(input_start, xin) 
     input_stop_ind = find_last_below(input_stop, xin)
-    # require at least one point in the window
-    @assert input_stop_ind >= input_start_ind
 
-    input_x = xin[input_start_ind:input_stop_ind]
-    input_y = @view yin[input_start_ind:input_stop_ind]
+    input_points = input_stop_ind - input_start_ind + 1
+    if input_points < required_input_points
+        #upsample
+        @assert !ismissing(upsampling_basis) "Not enough input points and no upsampling basis"
+        upsample_step = input_width/required_input_points
+        input_x = range(input_start + upsample_step/2, step=upsample_step, length=required_input_points)
+        in_step = Float64(xin.step)
+        input_y = [interpolate_point(xin, yin, up_x, in_step, in_step, upsampling_basis) for up_x in input_x] 
+        # TODO use buffer for upsampled values (length == required_input_values) that is allocated at a call to `regrid` instead of allocating every time
+    else # don't upsample
+        input_x = xin[input_start_ind:input_stop_ind]
+        input_y = @view yin[input_start_ind:input_stop_ind]
+    end
+
     left_slice_contribution = .5*slice_weighted_mean(xpoint, slice_width, input_x, input_y, basis)
 
     # right slice
@@ -100,20 +115,46 @@ function interpolate_point(xin, yin, xpoint, left_unit_width, right_unit_width, 
     input_width = slice_width*basis.width
     input_start = xpoint
     input_stop = xpoint + input_width
-    @assert input_start >= xin[1]
+    @assert input_start >= xin[1] "Not enough points at the beginning of the input"
     @assert input_stop <= xin[end] "Not enough points at the end of the input"
 
     #find relevant input points
     input_start_ind = find_first_above_or_equal(input_start, xin)
     input_stop_ind = find_last_below_or_equal(input_stop, xin)
-    # require at least one point in the window
-    @assert input_stop_ind >= input_start_ind "input range $(input_start) to $(input_stop) does not contain at least one point"
 
-    input_x = xin[input_start_ind:input_stop_ind]
-    input_y = @view yin[input_start_ind:input_stop_ind]
+    input_points = input_stop_ind - input_start_ind + 1
+    if input_points < required_input_points
+        #upsample
+        @assert !ismissing(upsampling_basis) "Not enough input points and no upsampling basis"
+        upsample_step = input_width/required_input_points
+        input_x = range(input_start + upsample_step/2, step=upsample_step, length=required_input_points)
+        in_step = Float64(xin.step)
+        input_y = [interpolate_point(xin, yin, xpoint, in_step, in_step, upsampling_basis) for xpoint in input_x] 
+    else # don't upsample
+        input_x = xin[input_start_ind:input_stop_ind]
+        input_y = @view yin[input_start_ind:input_stop_ind]
+    end
+
     right_slice_contribution = .5*slice_weighted_mean(xpoint, slice_width, input_x, input_y, basis)
     
     return left_slice_contribution + right_slice_contribution
+end
+
+function prepare_input_or_upsample()
+    input_points = input_stop_ind - input_start_ind + 1
+    if input_points < required_input_points
+        #upsample
+        @assert !ismissing(upsampling_basis) "Not enough input points and no upsampling basis"
+        upsample_step = input_width/required_input_points
+        input_x = range(input_start + upsample_step/2, step=upsample_step, length=required_input_points)
+        in_step = Float64(xin.step)
+        input_y = [interpolate_point(xin, yin, xpoint, in_step, in_step, HannBasis(3.)) for xpoint in input_x] 
+        # TODO change to Lanczos Basis
+    else # don't upsample
+        input_x = xin[input_start_ind:input_stop_ind]
+        input_y = @view yin[input_start_ind:input_stop_ind]
+    end
+    return (input_x, input_y)
 end
 
 function slice_weighted_mean(xpoint, slice_width, xin_points, yin_points, basis)
@@ -126,6 +167,9 @@ function slice_weighted_mean(xpoint, slice_width, xin_points, yin_points, basis)
         win_value::Float64 = basis.f(rel_pos)
         win_acc += win_value
         val_acc += y*win_value
+    end
+    if win_acc <= 0
+        error("shouldn't happen")
     end
     return val_acc / win_acc # normalization
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -35,7 +35,7 @@ using Statistics
         xin = 1.0:1.0:7.0
         yin = [1.,2.,3.,4.,5.,6.,7.]
         xout = [3.2, 5.2]
-        @test regrid(xin, yin, xout) == [3.5, 5.5]
+        @test regrid(xin, yin, xout, required_input_points=1) == [3.5, 5.5]
     end
 
     @testset "Basic Example 2" begin
@@ -45,10 +45,21 @@ using Statistics
         xout = [3.2, 6] # Δ = 2.8, Δ/2 = 1.4
         # first point: from 1.8 to 4.6 -> 2 3 | 4
         # second points: from 4.6 to 7.4 -> 5 |6 7 (middle one should be counted to the higher interval)
-        @test regrid(xin, yin, xout) == [
+        @test regrid(xin, yin, xout, required_input_points=1) == [
             mean([mean(yin[2:3]), yin[4]]), 
             mean([mean(yin[5:5]), mean(yin[6:7])])
             ]
+    end
+
+    @testset "Basic Example 3" begin
+        yin = [1.0, 212.47605778632067, 118.55093948572791, 273.88610509333614, 
+        307.67343485176576, 257.1875847477985, 357.4538672443729, 484.87523154603514, 
+        400.1796499550531, 398.29054790171745, 317.0589375578311, 501.18723362727246, 
+        608.6533266004108, 428.7113682873226, 869.7311001362467]
+        xin = range(0.0, step=0.18310546955466883, length=length(yin))
+        xout = [1.0, 1.0599939505656388]
+        output = regrid(xin, yin, xout)
+        @test all(.!isnan.(output))
     end
 
 end

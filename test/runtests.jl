657f9c9142dd6ce73b7de34bdde87186d936a3b7
Johannes Fried <jcmf.schule@gmail.com>
2022-07-20 19:50:54 +0200

    Add Aqua.jl
    closes # 8

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,14 @@ using NonuniformResampling1D
 using Test
 using Statistics
 using Interpolations
+using Aqua
 
 @testset "NonuniformResampling1D Tests" begin
     include("range_utilities.test.jl")
     include("window_functions.test.jl")
     include("nuresample.test.jl")
+
+    @testset "Aqua.jl" begin
+        Aqua.test_all(NonuniformResampling1D, ambiguities=(recursive=false))
+    end
 end
